0a6cfe72ea5bd7b5e10a90801280e8583056832d
wooxist <tossy@tossy.me>
2021-03-10 15:11:15 +0900

    update force update

--- remedi_update/lib/feature/force_update_viewmodel.dart
+++ remedi_update/lib/feature/force_update_viewmodel.dart
@@ -2,9 +2,8 @@ import 'package:remedi_update/remedi_update.dart';
 import 'package:store_redirect/store_redirect.dart';
 
 class ForceUpdateViewModel extends IForceUpdateViewModel {
-  IForceUpdateRepository repo;
-
-  ForceUpdateViewModel({this.repo}) : assert(repo != null);
+  ForceUpdateViewModel({IForceUpdateRepository repository})
+      : super(repo: repository);
 
   @override
   goToUpdate() {
@@ -13,11 +12,4 @@ class ForceUpdateViewModel extends IForceUpdateViewModel {
       iOSAppId: repository.iosAppId,
     );
   }
-
-  @override
-  // TODO: implement initState
-  ForceUpdateViewState get initState => ForceUpdateViewState.Init;
-
-  @override
-  IForceUpdateRepository get repository => repo;
 }
--- remedi_update/lib/viewmodel/i_force_update_viewmodel.dart
+++ remedi_update/lib/viewmodel/i_force_update_viewmodel.dart
@@ -3,7 +3,17 @@ import 'package:stacked_mvvm/stacked_mvvm.dart';
 
 abstract class IForceUpdateViewModel
     extends BaseViewModel<ForceUpdateViewState, IForceUpdateRepository> {
+  IForceUpdateRepository repo;
+
+  IForceUpdateViewModel({this.repo}) : assert(repo != null);
+
   goToUpdate();
+
+  @override
+  ForceUpdateViewState get initState => ForceUpdateViewState.Init;
+
+  @override
+  IForceUpdateRepository get repository => repo;
 }
 
 enum ForceUpdateViewState { Init }
