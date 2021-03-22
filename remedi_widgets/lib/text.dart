a2d2e6fab71d78791ac128d88a0f01ecf93684f2
wooxist <tossy@tossy.me>
2021-03-22 14:28:24 +0900

    Revert "support null-safety#remedi-widget"
    This reverts commit 7e8b596f8bd594ad188511adac62c5a6ec313add.

--- remedi_widgets/lib/text.dart
+++ remedi_widgets/lib/text.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 class FixedScaleText extends StatelessWidget {
   final Text text;
 
-  const FixedScaleText({Key? key, required this.text}) : super(key: key);
+  const FixedScaleText({Key key, this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
