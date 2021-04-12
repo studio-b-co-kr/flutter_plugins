0998989f2fe1c2c08ee88d4d89c8b4cf5732c003
daewook <tossy@remedi.co.kr>
2021-04-12 14:38:28 +0900

    engae example

--- remedi_engage/example/lib/main.dart
+++ remedi_engage/example/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
