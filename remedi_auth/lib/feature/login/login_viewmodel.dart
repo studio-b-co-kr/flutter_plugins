267d35c236133a433de1e9bfabf3df805d0ba00e
daewook <tossy@remedi.co.kr>
2021-04-16 14:25:03 +0900

    don't show error message if the error message of kakao is "cancelled.".

--- remedi_auth/lib/feature/login/login_viewmodel.dart
+++ remedi_auth/lib/feature/login/login_viewmodel.dart
@@ -177,7 +177,7 @@ class LoginViewModel extends ILoginViewModel {
       this.error =
           AuthError(title: title, code: code, message: message, error: error);
 
-      if (message == "cancelled") {
+      if (message == "cancelled.") {
         update(state: LoginViewState.Idle);
       } else {
         update(state: LoginViewState.Error);
