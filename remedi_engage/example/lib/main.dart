81e153ed669e261caed3be476988f7354f08ee52
daewook <tossy@remedi.co.kr>
2021-04-22 14:18:32 +0900

    wip#fcm_module

--- remedi_engage/example/lib/main.dart
+++ remedi_engage/example/lib/main.dart
@@ -1,9 +1,7 @@
 import 'dart:developer' as dev;
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:remedi_base/remedi_base.dart';
 import 'package:remedi_engage/remedi_engage.dart';
 
