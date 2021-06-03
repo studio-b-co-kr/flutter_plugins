fa2c7b65afa79af196f693c633398152af4dc3bc
daewook <tossy@remedi.co.kr>
2021-06-03 19:01:16 +0900

    add viewmodel factory

--- stacked_mvvm/lib/stacked_mvvm.dart
+++ stacked_mvvm/lib/stacked_mvvm.dart
@@ -10,11 +10,19 @@ import 'package:rxdart/rxdart.dart';
 import 'package:stacked/stacked.dart';
 
 part 'i_model.dart';
+
 part 'i_page.dart';
+
 part 'i_repository.dart';
-part 'i_view_model.dart';
-part 'i_widget.dart';
+
 part 'i_view.dart';
+
+part 'i_view_model.dart';
+
+part 'i_widget.dart';
+
 part 'list_view.dart';
+
 part 'state_data.dart';
+
 part 'viewmodel_factory.dart';
