4b885ce8ab065ce1f53ce7574719b8b6f4e7d486
daewook <tossy@remedi.co.kr>
2021-06-03 14:15:57 +0900

    change type of data in StateData to non-null

--- stacked_mvvm/lib/state_data.dart
+++ stacked_mvvm/lib/state_data.dart
@@ -1,10 +1,10 @@
 part of 'stacked_mvvm.dart';
 
 class StateData<D, S> {
-  D? data;
+  D data;
   S state;
 
-  StateData({this.data, required this.state});
+  StateData({required this.data, required this.state});
 
   StateData<D, S> clone() {
     return StateData(data: this.data, state: this.state);
