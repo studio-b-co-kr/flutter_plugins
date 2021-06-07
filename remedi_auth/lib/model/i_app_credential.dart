5501f2f92743740bb5ed9b166a82101bf027bbd5
daewook <tossy@remedi.co.kr>
2021-06-07 17:02:15 +0900

    update i_credential

--- remedi_auth/lib/model/i_app_credential.dart
+++ remedi_auth/lib/model/i_app_credential.dart
@@ -2,16 +2,21 @@ import 'package:remedi_auth/auth_error.dart';
 import 'package:remedi_base/remedi_base.dart';
 
 abstract class ICredential extends IDto {
-  final String? userId;
-  final String? accessToken;
+  final String userId;
+  final String userCode;
+  final String userRole;
+  final String accessToken;
   final String? refreshToken;
   final AuthError? error;
 
-  ICredential(
-      {required this.userId,
-      required this.accessToken,
-      this.refreshToken,
-      this.error});
+  ICredential({
+    required this.userId,
+    required this.userCode,
+    required this.userRole,
+    required this.accessToken,
+    this.refreshToken,
+    this.error,
+  });
 
   get isError => error != null;
 }
