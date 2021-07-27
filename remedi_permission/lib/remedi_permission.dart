169e8593884a89957e3a37530df0c6bb63b64f2b
daewook <tossy@remedi.co.kr>
2021-07-27 19:44:48 +0900

    change checking all granted

--- remedi_permission/lib/remedi_permission.dart
+++ remedi_permission/lib/remedi_permission.dart
@@ -3,6 +3,7 @@ library remedi_permission;
 import 'dart:developer' as dev;
 
 import 'package:permission_handler/permission_handler.dart';
+import 'package:remedi_permission/model/app_permission.dart';
 
 import 'data/local_storage.dart';
 
@@ -17,12 +18,15 @@ export 'features/permission_viewmodel.dart';
 export 'model/app_permission.dart';
 
 class PermissionManager {
-  static final List<Permission> _permissionList = [];
+  static final List<AppPermission> _appPermissionList = [];
 
-  static List<Permission> get permissionList => _permissionList;
+  static List<AppPermission> get permissionList => _appPermissionList;
 
   static init(List<Permission> permissionList) {
-    _permissionList.addAll(permissionList);
+    for (Permission permission in permissionList) {
+      AppPermission appPermission = AppPermission(permission);
+      _appPermissionList.add(appPermission);
+    }
   }
 
   static Future<bool> get doNotShowPermissionOnSplash async =>
@@ -30,11 +34,8 @@ class PermissionManager {
 
   static Future<bool> get allGranted async {
     bool ret = true;
-    await Future.forEach<Permission>(_permissionList, (permission) async {
-      PermissionStatus status = await permission.status;
-      ret = ret &&
-          (status == PermissionStatus.granted ||
-              status == PermissionStatus.limited);
+    await Future.forEach<AppPermission>(_appPermissionList, (permission) async {
+      ret = ret && await permission.isGranted;
     });
     dev.log("allGranted = $ret", name: "PermissionManager");
     return ret;
