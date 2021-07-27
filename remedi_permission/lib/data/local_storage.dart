c3323f72ad8dd2e6f3a2e7827ac49ec2a3be8c18
daewook <tossy@remedi.co.kr>
2021-07-27 16:04:32 +0900

    change local storage to shared preference

--- remedi_permission/lib/data/local_storage.dart
+++ remedi_permission/lib/data/local_storage.dart
@@ -1,6 +1,6 @@
 import 'dart:developer' as dev;
 
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalStorage {
   static final LocalStorage _instance = LocalStorage._();
@@ -11,14 +11,15 @@ class LocalStorage {
 
   static const _KEY_SKIP_ON_SPLASH = "KEY_SKIP_ON_SPLASH";
   static const _VALUE_SKIP = "skipped";
-  final FlutterSecureStorage _storage = FlutterSecureStorage();
 
   Future skip() async {
-    return await _storage.write(key: _KEY_SKIP_ON_SPLASH, value: _VALUE_SKIP);
+    var sp = await SharedPreferences.getInstance();
+    return await sp.setString(_KEY_SKIP_ON_SPLASH, _VALUE_SKIP);
   }
 
   Future<bool> get skipped async {
-    bool ret = await _storage.read(key: _KEY_SKIP_ON_SPLASH) == _VALUE_SKIP;
+    var sp = await SharedPreferences.getInstance();
+    bool ret = await await sp.getString(_KEY_SKIP_ON_SPLASH) == _VALUE_SKIP;
     dev.log("skipped = $ret", name: "PermissionManager");
     return ret;
   }
