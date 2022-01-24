515758c01a21c052d5360123f0ffc338eefcaa98
daewook <tossy@remedi.co.kr>
2022-01-24 17:19:07 +0900

    Update i_login_repository.dart
    change return type.

--- remedi_auth/lib/repository/i_login_repository.dart
+++ remedi_auth/lib/repository/i_login_repository.dart
@@ -1,12 +1,11 @@
 import 'package:remedi_auth/model/apple_credential.dart';
-import 'package:remedi_auth/model/i_app_credential.dart';
 import 'package:remedi_auth/model/kakao_credential.dart';
 import 'package:stacked_mvvm/stacked_mvvm.dart';
 
 abstract class ILoginRepository extends IRepository {
   /// loginWithApple must return null, AppCredential, AuthError
-  Future<ICredential> loginWithApple(AppleCredential appleCredential);
+  Future<dynamic> loginWithApple(AppleCredential appleCredential);
 
   /// loginWithKakao must return null, AppCredential, AuthError
-  Future<ICredential> loginWithKakao(KakaoCredential credential);
+  Future<dynamic> loginWithKakao(KakaoCredential credential);
 }
