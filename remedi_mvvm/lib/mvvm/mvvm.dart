dd85fe36e672ff50c49797b64412223049c4fec4
daewook <tossy@remedi.co.kr>
2022-03-16 11:10:27 +0900

    change naming

--- remedi_mvvm/lib/mvvm/mvvm.dart
+++ remedi_mvvm/lib/mvvm/mvvm.dart
@@ -6,8 +6,8 @@ import 'package:provider/provider.dart';
 import '../app/app.dart';
 
 part 'app_model.dart';
+part 'data_view.dart';
 part 'repository.dart';
-part 'state_view.dart';
-part 'view.dart';
+part 'state_data_view.dart';
 part 'view_model.dart';
 part 'view_model_view.dart';
