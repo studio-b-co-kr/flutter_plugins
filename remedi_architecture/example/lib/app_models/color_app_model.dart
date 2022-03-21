eb9c614c1747664f0e273a9a9749022ad1b7c9c3
daewook <tossy@remedi.co.kr>
2022-03-21 14:21:11 +0900

    wip#remedi_architecture

--- remedi_architecture/example/lib/app_models/color_app_model.dart
+++ remedi_architecture/example/lib/app_models/color_app_model.dart
@@ -3,14 +3,19 @@ import 'package:remedi_architecture/remedi_architecture.dart';
 
 class ColorAppModel extends IAppModel {
   ColorAppModel({bool? withInit}) : super(withInit: withInit);
+
+  /// Provider 를 통해서 제공되는 색깔 데이터
   Color color = Colors.purple;
 
+  /// Home Page 의 백그라운드 컬러 를 변경 한다.
   void toggleColor() {
     if (color == Colors.purple) {
       color = Colors.orange;
     } else {
       color = Colors.purple;
     }
+
+    /// 실시간으로 변경을 알려야 할 경우 notifyListeners() 를 호출 한다.
     notifyListeners();
   }
 
