bd29f221c85200f5b44cc1d9c1be07483abaa582
daewook <tossy@remedi.co.kr>
2022-03-29 15:11:27 +0900

    wip

--- remedi/lib/remedi.dart
+++ remedi/lib/remedi.dart
@@ -5,6 +5,7 @@ export 'package:provider/provider.dart';
 
 export 'app/app.dart';
 export 'architecture/architecture.dart';
+export 'engage/messaging/message.dart';
 export 'net/net.dart';
 export 'splash/splash.dart';
 export 'widgets/widgets.dart';
