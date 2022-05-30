295b868d08934e2269b53e7ab36f3b11bbbc03c4
daewook <tossy@remedi.co.kr>
2022-05-30 13:57:26 +0900

    update version

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/daewook.kwon/Library/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/Development/repo/remedi_flutter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.2"
+export "FLUTTER_BUILD_NUMBER=0.0.2"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
